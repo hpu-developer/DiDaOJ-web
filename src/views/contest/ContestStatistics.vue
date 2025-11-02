063a5820fc27ab2167ec937d7d2ba20f7d69fa04
BoilTask <BoilTask@qq.com>
2025-11-02 14:00:02 +0800

    完善接口

--- src/views/contest/ContestStatistics.vue
+++ src/views/contest/ContestStatistics.vue
@@ -2,7 +2,7 @@
 import { nextTick, WatchStopHandle } from "vue";
 import { onBeforeUnmount, onMounted, ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
-import { ShowErrorTips, useCurrentInstance } from "@/util";
+import { ShowErrorTips, ShowTextTipsInfo, useCurrentInstance } from "@/util";
 import { GetContestProblemIndexStr, GetContestStatistics } from "@/apis/contest.ts";
 
 import { GetJudgeStatusColor, GetJudgeStatusStr, IsJudgeStatusRunning, JudgeStatus } from "@/apis/judge.ts";
@@ -13,6 +13,8 @@ import { PieChart, LineChart } from "echarts/charts";
 import { LabelLayout, UniversalTransition } from "echarts/features";
 import { CanvasRenderer } from "echarts/renderers";
 import { handleGotoContestProblem } from "@/util/router.ts";
+import { ProblemRankType } from "@/apis/problem.ts";
+import { GetJudgeLanguageStr, GetSubmitLanguages, JudgeLanguage } from "@/apis/language.ts";
 
 echarts.use([
   TitleComponent,
@@ -38,19 +40,160 @@ let contestId = 0;
 const contestLoading = ref(false);
 const contestData = ref([]);
 
+const allOptions = [{ label: "全部语言", value: undefined }] as { label: string; value: JudgeLanguage | undefined }[];
+const languageOptions = ref([] as { label: string; value: JudgeLanguage | undefined }[]);
+languageOptions.value = GetSubmitLanguages();
+languageOptions.value = allOptions.concat(languageOptions.value);
+
+const searchForm = ref({
+  language: undefined as JudgeLanguage | undefined,
+});
+
 const listColumns = ref([] as any[]);
+
+const loadCountStatistics = (countData: { [key: string]: number }) => {
+  let dateValues = [];
+  let acceptValues = [];
+  let attemptValues = [];
+  let lastAcceptValue = 0;
+  let lastAttemptValue = 0;
+  for (const item of countData) {
+    const date = new Date(item.date);
+    const dataDayString = date.toLocaleString();
+    dateValues.push(dataDayString);
+    acceptValues.push(item.accept + lastAcceptValue);
+    attemptValues.push(item.attempt + lastAttemptValue);
+    lastAcceptValue += item.accept;
+    lastAttemptValue += item.attempt;
+  }
+
+  const chartDom = document.getElementById("count-statistics-div");
+  const myChart = echarts.init(chartDom);
+  const option = {
+    title: {
+      text: "提交数量",
+    },
+    tooltip: {
+      trigger: "axis",
+      axisPointer: {
+        type: "cross",
+        label: {
+          backgroundColor: "#6a7985",
+        },
+      },
+    },
+    legend: {
+      data: ["Accept", "Attempt"],
+    },
+    toolbox: {
+      feature: {
+        saveAsImage: {},
+      },
+    },
+    xAxis: [
+      {
+        type: "category",
+        boundaryGap: false,
+        data: dateValues,
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+      },
+    ],
+    series: [
+      {
+        name: "Accept",
+        type: "line",
+        stack: "Total",
+        color: "#67C23A",
+        areaStyle: {},
+        emphasis: {
+          focus: "series",
+        },
+        data: acceptValues,
+      },
+      {
+        name: "Attempt",
+        type: "line",
+        stack: "Total",
+        color: "#409EFF",
+        label: {
+          show: true,
+          position: "top",
+        },
+        areaStyle: {},
+        emphasis: {
+          focus: "series",
+        },
+        data: attemptValues,
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+};
+
+const loadLanguageStatistics = (languageData: { [key: string]: number }) => {
+  const chartDom = document.getElementById("language-statistics-div");
+  const myChart = echarts.init(chartDom);
+
+  let dataList = [] as { name: string; value: number }[];
+  Object.keys(languageData).forEach((key) => {
+    dataList.push({
+      name: GetJudgeLanguageStr(Number(key)),
+      value: languageData[key],
+    });
+  });
+
+  const option = {
+    title: {
+      text: "语言使用统计",
+      left: "center",
+    },
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      orient: "vertical",
+      left: "left",
+    },
+    series: [
+      {
+        name: "语言",
+        type: "pie",
+        radius: "50%",
+        data: dataList,
+        emphasis: {
+          itemStyle: {
+            shadowBlur: 10,
+            shadowOffsetX: 0,
+            shadowColor: "rgba(0, 0, 0, 0.5)",
+          },
+        },
+      },
+    ],
+  };
+
+  myChart.setOption(option);
+};
 
 const fetchContestData = async () => {
   contestLoading.value = true;
 
-  const res = await GetContestStatistics(contestId, undefined);
+  const res = await GetContestStatistics(contestId, searchForm.value.language);
   if (res.code !== 0) {
     contestLoading.value = false;
     ShowErrorTips(globalProperties, res.code);
     await router.push({ name: "contest" });
     return;
   }
-
+  if (!res.data.has_auth) {
+    ShowTextTipsInfo(globalProperties, "您目前没有权限查看比赛统计");
+    await router.push({ name: "contest-detail", params: { contestId: contestId } });
+    return;
+  }
   const statisticsList = res.data.statistics;
 
   statisticsList.sort((a, b) => {
@@ -74,104 +217,8 @@ const fetchContestData = async () => {
     contestData.value.push(elment);
   });
 
-
-  var chartDom = document.getElementById('main');
-  var myChart = echarts.init(chartDom);
-  var option;
-
-  option = {
-    title: {
-      text: 'Stacked Area Chart'
-    },
-    tooltip: {
-      trigger: 'axis',
-      axisPointer: {
-        type: 'cross',
-        label: {
-          backgroundColor: '#6a7985'
-        }
-      }
-    },
-    legend: {
-      data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
-    },
-    xAxis: [
-      {
-        type: 'category',
-        boundaryGap: false,
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      }
-    ],
-    yAxis: [
-      {
-        type: 'value'
-      }
-    ],
-    series: [
-      {
-        name: 'Email',
-        type: 'line',
-        stack: 'Total',
-        areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        },
-        data: [120, 132, 101, 134, 90, 230, 210]
-      },
-      {
-        name: 'Union Ads',
-        type: 'line',
-        stack: 'Total',
-        areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        },
-        data: [220, 182, 191, 234, 290, 330, 310]
-      },
-      {
-        name: 'Video Ads',
-        type: 'line',
-        stack: 'Total',
-        areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        },
-        data: [150, 232, 201, 154, 190, 330, 410]
-      },
-      {
-        name: 'Direct',
-        type: 'line',
-        stack: 'Total',
-        areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        },
-        data: [320, 332, 301, 334, 390, 330, 320]
-      },
-      {
-        name: 'Search Engine',
-        type: 'line',
-        stack: 'Total',
-        label: {
-          show: true,
-          position: 'top'
-        },
-        areaStyle: {},
-        emphasis: {
-          focus: 'series'
-        },
-        data: [820, 932, 901, 934, 1290, 1330, 1320]
-      }
-    ]
-  };
-
-  option && myChart.setOption(option);
-
+  loadCountStatistics(res.data.count);
+  loadLanguageStatistics(res.data.language);
 
   nextTick(function () {
     const allDoms = document.getElementsByClassName("echart-statistics");
@@ -218,6 +265,14 @@ const fetchContestData = async () => {
   });
 
   contestLoading.value = false;
+};
+
+const handleSearchFormSubmit = () => {
+  fetchContestData();
+};
+
+const handleSearchFormReset = () => {
+  searchForm.value.language = undefined;
 };
 
 onMounted(async () => {
@@ -298,9 +353,23 @@ onBeforeUnmount(() => {
       </t-card>
     </t-col>
     <t-col :span="4">
-      <div style="margin: 10px">
-        <t-card class="sh-card"><div id="main" style="min-height: 400px"></div></t-card>
-      </div>
+      <t-card class="sh-card">
+        <t-form layout="inline" @submit="handleSearchFormSubmit" @reset="handleSearchFormReset">
+          <t-form-item label="语言">
+            <t-select v-model="searchForm.language" placeholder="请选择提交语言" auto-width clearable>
+              <t-option v-for="item in languageOptions" :key="item.value" :value="item.value" :label="item.label"></t-option>
+            </t-select>
+          </t-form-item>
+          <t-form-item>
+            <t-space>
+              <t-button theme="primary" type="submit">搜索</t-button>
+              <t-button theme="default" variant="base" type="reset">重置</t-button>
+            </t-space>
+          </t-form-item>
+        </t-form>
+      </t-card>
+      <t-card class="sh-card"><div id="count-statistics-div" style="min-height: 400px"></div></t-card>
+      <t-card class="sh-card"><div id="language-statistics-div" style="min-height: 400px"></div></t-card>
     </t-col>
   </t-row>
 </template>
